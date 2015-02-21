d04595f1d7373d4497b69d050f9d42b0715907a4
sashwat <sashwat@BLR2-6900092790.ant.amazon.com>
2015-02-21 17:15:01 +0530

    Addinal main code

---
+++ WorldTravellersChallenge/src/main/java/com/avengers/constants/BeanIdentifiers.java
@@ -0,0 +1,20 @@
+package com.avengers.constants;
+
+public class BeanIdentifiers {
+
+	public static final String RESTRICTRED_COUNTRY_CITIES_SET = "restrictedCountryCitiesSet";
+
+	public static final String RESTRICTRED_COUNTRIES_SET = "restrictedCountriesSet";
+
+	public static final String VISITED_CITIES = "VisitedCities";
+
+	public static final String WANT_TO_VISIT_CITIES = "wantToVisitCities";
+
+	public static final String DESTINATION_PATHS_MAP = "CountryCityPathsMap";
+
+	public static final String COUNTRY_CITY_PATHS_REVERSE_MAP = "CountryCityPathsReversemMap";
+
+	public static final String DEPENDENT_PATHS = "DependentPaths";
+
+	public static final String APPLICATION_INITIALIZER = "ApplicationInitializer";
+}
--- WorldTravellersChallenge/src/main/java/com/avengers/main/Main.java
+++ WorldTravellersChallenge/src/main/java/com/avengers/main/Main.java
@@ -1,5 +1,8 @@
 package com.avengers.main;
 
 public class Main {
+	public static void main(String args[]) {
+		
+	}
 
 }
