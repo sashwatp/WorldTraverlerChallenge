2125d41ed0728cf608fd1f3482ad41c16881d318
NavyataGudaraddi <navyatarg@gmail.com>
2015-02-21 15:47:33 +0530

    model changes

--- WorldTravellersChallenge/src/main/java/com/avengers/model/PathSet.java
+++ WorldTravellersChallenge/src/main/java/com/avengers/model/PathSet.java
@@ -1,11 +1,40 @@
 package com.avengers.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class PathSet {
-	
-	private Set<Path> Paths;
-	private Path shortestRestrictedPath;
-	private Path shortestUnrestrictedPath;
+
+    private Set<Path> Paths;
+    private Path shortestRestrictedPath;
+    private Path shortestUnrestrictedPath;
+
+    public PathSet() {
+        Paths = new HashSet<Path>();
+        shortestRestrictedPath = null;
+        shortestUnrestrictedPath = null;
+    }
+
+    public void addPath(Path path) {
+        if (Paths.add(path)) {
+            if (path.isValid()) {
+                if (path.HasRestricted()) {
+                    if (shortestRestrictedPath == null) {
+                        shortestRestrictedPath = path;
+
+                    } else if (shortestRestrictedPath.getPathLength() > path.getPathLength()) {
+                        shortestRestrictedPath = path;
+
+                    }
+
+                } else if (shortestUnrestrictedPath == null) {
+                    shortestUnrestrictedPath = path;
+                } else if (shortestUnrestrictedPath.getPathLength() > path.getPathLength()) {
+                    shortestUnrestrictedPath = path;
+                }
+            }
+        }
+
+    }
 
 }
