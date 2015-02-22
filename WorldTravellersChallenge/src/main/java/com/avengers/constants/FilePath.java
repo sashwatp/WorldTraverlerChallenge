2cd64ae408b3c32eefc2fd4e837c742be162502f
Medha-K <hkanchib@CNU31897PT.nsn-intra.net>
2015-02-22 11:24:30 +0530

    Adding resources test file and tostring for path

--- WorldTravellersChallenge/src/main/java/com/avengers/constants/FilePath.java
+++ WorldTravellersChallenge/src/main/java/com/avengers/constants/FilePath.java
@@ -2,13 +2,13 @@ package com.avengers.constants;
 
 public class FilePath {
 
-	public static final String VISITED_COUTRY_CITIES = "Visited.txt";
+	public static final String VISITED_COUTRY_CITIES = "src/main/resources/Visited.txt";
 
-	public static final String RESTRICTED_COUNTIRES = "RestrictedCountries.txt";
+	public static final String RESTRICTED_COUNTIRES = "src/main/resources/RestrictedCountries.txt";
 
-	public static final String RESTRICTED_COUNTRY_CITIES = "RestrictedCities.txt";
+	public static final String RESTRICTED_COUNTRY_CITIES = "src/main/resources/RestrictedCities.txt";
 
-	public static final String WANT_TO_VISIT = "WantToVisit.txt";
+	public static final String WANT_TO_VISIT = "src/main/resources/WantToVisit.txt";
 
-	public static final String DEPENDENT = "Dependent.txt";
+	public static final String DEPENDENT = "src/main/resources/Dependent.txt";
 }
