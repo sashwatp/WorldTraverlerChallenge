ad3069f1b9aa5b449b71134f69203e7afb1c7431
sashwat <sashwat@BLR2-6900092790.ant.amazon.com>
2015-02-22 11:48:37 +0530

    IsValidCountryOrCity

--- WorldTravellersChallenge/src/main/java/com/avengers/parser/CountryCityParser.java
+++ WorldTravellersChallenge/src/main/java/com/avengers/parser/CountryCityParser.java
@@ -9,11 +9,23 @@ public class CountryCityParser {
 
         String[] temp = countryCityString.split(":");
         if (temp.length == 2) {
-            countryCity.setCountry(temp[0].trim());
-            countryCity.setCity(temp[1].trim());
+            String country = temp[0].trim();
+            String city = temp[1].trim();
+            if (!isValidCountryOrCity(country) || isValidCountryOrCity(city)) {
+                throw new RuntimeException("Invalid Country or City");
+            }
+            countryCity.setCountry(country);
+            countryCity.setCity(city);
         } else {
             throw new RuntimeException("Invalid Country city String");
         }
         return countryCity;
+    }
+
+    public static boolean isValidCountryOrCity(String input) {
+        if (input.matches("[a-ZA-Z]")) {
+            return true;
+        }
+        return false;
     }
 }
