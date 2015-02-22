03bc266d72d842b9f2e72c305bce68ae75b37104
sashwat <sashwat@BLR2-6900092790.ant.amazon.com>
2015-02-22 10:53:09 +0530

    Changes to toString method of CountryCity

--- WorldTravellersChallenge/src/main/java/com/avengers/model/CountryCity.java
+++ WorldTravellersChallenge/src/main/java/com/avengers/model/CountryCity.java
@@ -2,71 +2,76 @@ package com.avengers.model;
 
 public class CountryCity {
 
-	private String country;
-	private String city;
-	private boolean isVisited;
-	private boolean isRestricted;
+    private String country;
+    private String city;
+    private boolean isVisited;
+    private boolean isRestricted;
 
-	public String getCountry() {
-		return country;
-	}
+    public String getCountry() {
+        return country;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public boolean isVisited() {
-		return isVisited;
-	}
+    public boolean isVisited() {
+        return isVisited;
+    }
 
-	public void setVisited(boolean isVisited) {
-		this.isVisited = isVisited;
-	}
+    public void setVisited(boolean isVisited) {
+        this.isVisited = isVisited;
+    }
 
-	public boolean isRestricted() {
-		return isRestricted;
-	}
+    public boolean isRestricted() {
+        return isRestricted;
+    }
 
-	public void setRestricted(boolean isRestricted) {
-		this.isRestricted = isRestricted;
-	}
+    public void setRestricted(boolean isRestricted) {
+        this.isRestricted = isRestricted;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((country == null) ? 0 : country.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((city == null) ? 0 : city.hashCode());
+        result = prime * result + ((country == null) ? 0 : country.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CountryCity other = (CountryCity) obj;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		} else if (!city.equals(other.city))
-			return false;
-		if (country == null) {
-			if (other.country != null)
-				return false;
-		} else if (!country.equals(other.country))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CountryCity other = (CountryCity) obj;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        } else if (!city.toLowerCase().equals(other.city.toLowerCase()))
+            return false;
+        if (country == null) {
+            if (other.country != null)
+                return false;
+        } else if (!country.toLowerCase().equals(other.country.toLowerCase()))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return country + ":" + city;
+    }
 }
